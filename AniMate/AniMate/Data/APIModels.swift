c70b790f22439d2e7086d160c1264dfeba0af10c
Treisy-dev <vip.protop2004@gmail.com>
2023-07-06 03:00:20 +0300

    [add] add data files

---
+++ AniMate/AniMate/Data/APIModels.swift
@@ -0,0 +1,8 @@
+//
+//  APIModels.swift
+//  AniMate
+//
+//  Created by Кирилл Щёлоков on 06.07.2023.
+//
+
+import Foundation
---
+++ AniMate/AniMate/Data/NetworkService.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkService.swift
+//  AniMate
+//
+//  Created by Кирилл Щёлоков on 06.07.2023.
+//
+
+import Foundation
