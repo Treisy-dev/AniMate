d4dc345b99e3204f2273d6d413dbf829f272bff7
Treisy-dev <vip.protop2004@gmail.com>
2023-07-07 03:00:05 +0300

    [add] add new branch and API work

--- AniMate/AniMate/Data/APIModels.swift
+++ AniMate/AniMate/Data/APIModels.swift
@@ -6,3 +6,127 @@
 //
 
 import Foundation
+
+struct Anime: Codable {
+    let id: String
+    let type: String
+    //let links: Links
+    let attributes: Attributes
+    //let relationships: Relationships
+    
+    /*struct Links: Codable {
+        let selfLink: String
+        
+        enum CodingKeys: String, CodingKey {
+            case selfLink = "self"
+        }
+    }*/
+    
+    struct Attributes: Codable {
+        //let createdAt: String
+        //let updatedAt: String
+        let slug: String
+        //let synopsis: String
+        let description: String
+        //let coverImageTopOffset: Int
+        //let titles: Titles
+        //let canonicalTitle: String
+        //let abbreviatedTitles: [String]
+        let averageRating: String
+        //let ratingFrequencies: [String: String]
+        //let userCount: Int
+        //let favoritesCount: Int
+        let startDate: String
+        let endDate: String?
+        //let nextRelease: String?
+        //let popularityRank: Int
+        //let ratingRank: Int
+        //let ageRating: String
+        let ageRatingGuide: String
+        //let subtype: String
+        let status: String
+        //let tba: String?
+        let posterImage: PosterImage
+        //let coverImage: CoverImage
+        let episodeCount: Int?
+        let episodeLength: Int?
+        let totalLength: Int?
+        let youtubeVideoId: String?  //youtube trailer link
+        //let showType: String
+        //let nsfw: Bool
+        
+        /*struct Titles: Codable {
+            let en: String
+            let en_jp: String
+            let ja_jp: String
+        }*/
+        
+        struct PosterImage: Codable {
+            let tiny: String
+            //let large: String
+            let small: String
+            //let medium: String
+            //let original: String
+            //let meta: Meta
+            
+            /*struct Meta: Codable {
+                let dimensions: [String: Dimension]
+                
+                struct Dimension: Codable {
+                    let width: Int
+                    let height: Int
+                }
+            }*/
+        }
+        
+        /*struct CoverImage: Codable {
+            let tiny: String
+            let large: String
+            let small: String
+            let original: String
+            let meta: Meta
+            
+            struct Meta: Codable {
+                let dimensions: [String: Dimension]
+                
+                struct Dimension: Codable {
+                    let width: Int
+                    let height: Int
+                }
+            }
+        }*/
+    }
+    
+    /*struct Relationships: Codable {
+        //let genres: Relationship
+        //let categories: Relationship
+        //let castings: Relationship
+        //let installments: Relationship
+        //let mappings: Relationship
+        //let reviews: Relationship
+        //let mediaRelationships: Relationship
+        //let characters: Relationship
+        //let staff: Relationship
+        //let productions: Relationship
+        let quotes: Relationship
+        let episodes: Relationship
+        let streamingLinks: Relationship
+        let animeProductions: Relationship
+        let animeCharacters: Relationship
+        let animeStaff: Relationship
+        
+        struct Relationship: Codable {
+            let links: Links
+            
+            struct Links: Codable {
+                let selfLink: String
+                let related: String
+                
+                enum CodingKeys: String, CodingKey {
+                    case selfLink = "self"
+                    case related
+                }
+            }
+        }
+    }*/
+}
--- AniMate/AniMate/Data/NetworkService.swift
+++ AniMate/AniMate/Data/NetworkService.swift
@@ -6,3 +6,43 @@
 //
 
 import Foundation
+import Alamofire
+
+final class NetworkService{
+    
+    static let shared = NetworkService()
+        
+    private init() {}
+    
+    public func getWordInfo(for word: String, completion: @escaping (Result<[Anime], Error>) -> Void)  {
+        makeRequest(with: word) { result in
+            switch result {
+            case .success(let words):
+                completion(.success(words))
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+    }
+    
+    
+    private func makeRequest(with word: String,  completion: @escaping (Result<[Anime], Error>) -> Void) {
+        AF.request(makeUrl(word: word), method: .get).responseData { response in
+            switch response.result {
+            case .success(let word):
+                do{
+                    let result = try JSONDecoder().decode([Anime].self, from: word)
+                    completion(.success(result))
+                } catch let error{
+                    completion(.failure(error))
+                }
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+    }
+    
+    public func makeUrl(word: String) -> String{
+        return "https://kitsu.io/api/edge/anime?filter[text]=\(word)&page[limit]=5"
+    }
+}
