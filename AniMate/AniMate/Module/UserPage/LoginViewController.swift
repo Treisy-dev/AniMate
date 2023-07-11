71bffe469f4f6c89140298364db9d1ea5bb08e98
Jinx <DVStanchu@stud.kpfu.ru>
2023-07-11 16:30:15 +0300

    userPage

---
+++ AniMate/AniMate/Module/UserPage/LoginViewController.swift
@@ -0,0 +1,24 @@
+//
+//  LoginViewController.swift
+//  AniMate
+//
+//  Created by Данил on 11.07.2023.
+//
+
+import UIKit
+
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var loginTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBAction func registerNewUser(_ sender: Any) {
+    }
+    
+    @IBAction func loginExistUser(_ sender: Any) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
